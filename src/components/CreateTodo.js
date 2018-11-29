aad51f1551f1062594aba446bb865a8b6fccc93c
Kin Chan <xkinchanx@gmail.com>
2018-11-29 12:45:56 -0800

    [master] template

---
+++ src/components/CreateTodo.js
@@ -0,0 +1,11 @@
+
+/* It's too dangerous to go alone!  Here, take this:
+
+<div>
+  <form action="" onSubmit={this.addTodo}>
+    <input type="text" onChange={this.updateItem}/>
+    <button type="submit">add</button>
+  </form>
+</div>
+
+*/
